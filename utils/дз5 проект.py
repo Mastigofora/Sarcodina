a3149e3ea37bea97f91f9090a38eeac0c778b8e2
Mastigofora <skynadia@gmail.com>
2023-03-16 18:36:44 +0300

    ДЗ-4

---
+++ utils/дз5 проект.py
@@ -0,0 +1,8 @@
+sum = float(input())
+bills= [5000, 1000, 500, 100, 50, 10, 5, 2, 1, 0.50, 0.10, 0.05, 0.01]
+
+for a in bills:
+    c = sum // a
+    sum = sum % a
+    if c != 0:
+        print(a, "=", c, end=' ')
